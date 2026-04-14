8d1e80e0f4095bf97067bacf6de9800277de69a3
reyesmma <reyesmma@mail.uc.edu>
2026-04-13 21:02:27 -0400

    Initial commit

---
+++ src/Listmaker.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class ListMaker
+{
+    ArrayList<String> lines = new ArrayList<String>();
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+}
